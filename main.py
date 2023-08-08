684485bf93aab1e67d5a219c68edb1098311cb12
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-07 23:40:23 -0500

    add priunt

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import os
 
 # Set your API key and video ID
 API_KEY = os.environ["API_KEY_YOUTUBE_DATA"]
+print(API_KEY)
 VIDEO_ID = "5EfFqAAWvqw"
 
 # Create a YouTube service object
 youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=API_KEY)
-
+print(youtube)
 # Call the captions.list method
 captions_list_response = youtube.captions().list(
     part="snippet",
