309c9921c3b89388b3f47ee7c5fa1fd5f3a178dd
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-08 01:25:17 -0500

    youtube

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_limiter import Limiter
+from apiclient.discovery import build
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
 import logging
 
 logging.basicConfig(
@@ -29,6 +32,35 @@ def protected_resource():
 
     if api_key == 'tu_clave_secreta':
         return jsonify(message='Acceso concedido a la API protegida')
+    else:
+        return jsonify(error='Acceso no autorizado'), 401
+
+
+@app.route('/yutube_transcript', methods=['GET'])
+@api_key_limiter.limit("5 per minute")
+def youtube_transcript():
+    API_KEY = os.environ["API_KEY_YOUTUBE_DATA"]
+    SECRET_KEY_API = os.environ["SECRET_KEY_API"]
+
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    api_key = request.headers.get('X-API-Key')
+    logger = logging.getLogger('app')  # Crear un nuevo logger aquí
+    logger.info(f"Solicitud recibida para la ruta /api/resource con clave API: {api_key}")
+
+    if api_key == 'SECRET_KEY_API':
+        VIDEO_ID = request.args.get('video_id')
+        try:
+            responses = YouTubeTranscriptApi.get_transcript(VIDEO_ID, languages=['es'])
+            logger.info('\n'+"Video: "+"https://www.youtube.com/watch?v="+str(VIDEO_ID)+'\n'+'\n'+"Captions:")
+            text = ""
+            for response in responses:
+                text = text + " " + response['text']
+            response = {'result': text}
+        except Exception as e:
+            response = {'result': "error"}
+            logger.info(e)
+        
+        return jsonify(response)
     else:
         return jsonify(error='Acceso no autorizado'), 401
 
