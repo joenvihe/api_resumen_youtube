40a84ef35cf469f1df29cc5eb74b858c67d5b2e3
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-08 01:58:27 -0500

    open ai

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_limiter import Limiter
 from apiclient.discovery import build
 from youtube_transcript_api import YouTubeTranscriptApi
+import openai
 import os
 import logging
 
@@ -41,6 +42,8 @@ def protected_resource():
 def youtube_transcript():
     API_KEY = os.environ["API_KEY_YOUTUBE_DATA"]
     SECRET_KEY_API = os.environ["SECRET_KEY_API"]
+    openai.api_key = os.environ["TOKEN_OPENAI_CHATGPT"]
+    # Set your OpenAI API key
 
     youtube = build('youtube', 'v3', developerKey=API_KEY)
     api_key = request.headers.get('X-API-Key')
@@ -55,7 +58,11 @@ def youtube_transcript():
             text = ""
             for response in responses:
                 text = text + " " + response['text']
-            response = {'result': text}
+            
+            # Summarize the text
+            mt = len(text)/2
+            summary = openai.summarize(text, max_tokens=mt)  
+            response = {'result': text,'summary':summary}
         except Exception as e:
             response = {'result': "error"}
             logger.info(e)
@@ -67,25 +74,3 @@ def youtube_transcript():
 
 if __name__ == '__main__':
     app.run(port=5001)
-
-"""
-from apiclient.discovery import build
-from youtube_transcript_api import YouTubeTranscriptApi
-import os
- 
-API_KEY = os.environ["API_KEY_YOUTUBE_DATA"]
-# Set your API key and video ID
-print(API_KEY)
-VIDEO_ID = "5EfFqAAWvqw"
-channel_id = 'Your Channel_id'  # replace it with your channel id
-youtube = build('youtube', 'v3', developerKey=API_KEY)
-
-try:
-    responses = YouTubeTranscriptApi.get_transcript(VIDEO_ID, languages=['es'])
-    print('\n'+"Video: "+"https://www.youtube.com/watch?v="+str(VIDEO_ID)+'\n'+'\n'+"Captions:")
-    for response in responses:
-        text = response['text']
-        print(text)
-except Exception as e:
-    print(e)
-"""
