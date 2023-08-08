66c21608ee1c69b049b9feb2f333b36dc00d32ce
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-07 23:49:17 -0500

    VIDEO_ID

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ channel_id = 'Your Channel_id'  # replace it with your channel id
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 try:
-    responses = YouTubeTranscriptApi.get_transcript(video_id, languages=['es'])
-    print('\n'+"Video: "+"https://www.youtube.com/watch?v="+str(video_id)+'\n'+'\n'+"Captions:")
+    responses = YouTubeTranscriptApi.get_transcript(VIDEO_ID, languages=['es'])
+    print('\n'+"Video: "+"https://www.youtube.com/watch?v="+str(VIDEO_ID)+'\n'+'\n'+"Captions:")
     for response in responses:
         text = response['text']
         print(text)
